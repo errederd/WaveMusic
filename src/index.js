6072ecd9037e39f14e32d2c1c5792590b8e4fd73
elederd <mjjg140698@gmail.com>
2024-08-15 01:16:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const { ShardingManager } = require('discord.js');
 const fs = require('fs');
 const config = require('./config.js');
 const Logger = require('./structures/Logger.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
 
 const logger = new Logger();
 
@@ -35,38 +34,7 @@ manager
     });
 
 manager.on('shardCreate', shard => {
-    shard.on('ready', async () => {
+    shard.on('ready', () => {
         logger.start(`[CLIENT] Shard ${shard.id} connected to Discord's Gateway.`);
-
-        // Aquí debes especificar el ID del canal de voz al que quieres unirte
-        const channelId = 'TU_CANAL_DE_VOZ_ID';
-        const guildId = 'TU_GUILD_ID';
-
-        const channel = shard.client.channels.cache.get(channelId);
-
-        if (channel && channel.isVoice()) {
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: guildId,
-                adapterCreator: channel.guild.voiceAdapterCreator,
-            });
-
-            connection.on(VoiceConnectionStatus.Ready, () => {
-                logger.start(`[CLIENT] Conectado al canal de voz ${channel.name}.`);
-
-                // Reproducir el archivo de sonido
-                const player = createAudioPlayer();
-                const resource = createAudioResource('./src/utils/sonics.ogg');
-
-                player.play(resource);
-                connection.subscribe(player);
-
-                player.on(AudioPlayerStatus.Idle, () => {
-                    connection.destroy(); // Desconectar después de que se haya reproducido el sonido
-                });
-            });
-        } else {
-            logger.error('[CLIENT] No se encontró el canal de voz.');
-        }
     });
 });
