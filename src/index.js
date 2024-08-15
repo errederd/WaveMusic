3db8ebe1e72d22914fa8c13c28fee9f036917bb8
elederd <mjjg140698@gmail.com>
2024-08-15 02:13:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const { ShardingManager } = require('discord.js');
 const fs = require('fs');
 const config = require('./config.js');
 const Logger = require('./structures/Logger.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
 
 const logger = new Logger();
 
@@ -35,40 +34,7 @@ manager
     });
 
 manager.on('shardCreate', shard => {
-    shard.on('ready', async () => {
+    shard.on('ready', () => {
         logger.start(`[CLIENT] Shard ${shard.id} connected to Discord's Gateway.`);
-
-        // Se espera a que el bot esté listo
-        shard.client.on('ready', async () => {
-            logger.start(`[CLIENT] Client ready, processing voice connection.`);
-
-            const channelId = 'TU_CANAL_DE_VOZ_ID';
-            const guildId = 'TU_GUILD_ID';
-            const channel = shard.client.channels.cache.get(channelId);
-
-            if (channel && channel.isVoice()) {
-                const connection = joinVoiceChannel({
-                    channelId: channel.id,
-                    guildId: guildId,
-                    adapterCreator: channel.guild.voiceAdapterCreator,
-                });
-
-                connection.on(VoiceConnectionStatus.Ready, () => {
-                    logger.start(`[CLIENT] Conectado al canal de voz ${channel.name}.`);
-
-                    const player = createAudioPlayer();
-                    const resource = createAudioResource('./src/utils/sonics.ogg');
-
-                    player.play(resource);
-                    connection.subscribe(player);
-
-                    player.on(AudioPlayerStatus.Idle, () => {
-                        connection.destroy();
-                    });
-                });
-            } else {
-                logger.error('[CLIENT] No se encontró el canal de voz.');
-            }
-        });
     });
 });
